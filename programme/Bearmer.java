afb8c1373a43921816d0cd0f583020a2212a3b01
calbatorix <espinasse.pierre@outlook.fr>
2017-12-13 15:24:26 +0100

    debut teleporteur

---
+++ programme/Bearmer.java
@@ -0,0 +1,21 @@
+
+/**
+ * Décrivez votre classe Bearmer ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class Bearmer extends Item
+{
+    private boolean aCharge;
+
+    public Bearmer(final String pDescription, final double pPrix, final double pPoids, final boolean pCharge){
+        super(pDescription,pPrix,pPoids);
+        this.aCharge = pCharge;
+    }
+
+    public boolean isCharged(){return this.aCharge;}
+
+    public void charge(){this.aCharge = true;}
+    public void discharge(){this.aCharge = false;} 
+}
